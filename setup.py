19dc15eb49f9bb52f75dd8c5746f568407175754
Jonathan Sick <jonathansick@mac.com>
2012-05-05 18:34:38 -0400

    Starting the daophot package

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup
+from sphinx.setup_command import BuildDoc
+
+
+dependencies = """
+sphinx
+"""
+
+cmdclass = {'build_sphinx': BuildDoc}
+
+setup(name='pydaophot',
+    version="0.0.1",
+    author='Jonathan Sick',
+    author_email='jonathansick@mac.com',
+    description='DAOPHOT interface for python',
+    license='BSD',
+    install_requires=dependencies.split(),
+    cmdclass=cmdclass,
+    packages=['pydaophot']
+)
