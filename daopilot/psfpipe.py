8633bd547382fb69d7d329d2ddeeb35c40a0a479
Jonathan Sick <jonathansick@mac.com>
2012-11-16 19:05:49 -0500

    Simplified StarPicker interface

--- daopilot/psfpipe.py
+++ daopilot/psfpipe.py
@@ -16,11 +16,15 @@ from allstar import Allstar
 
 class PSFFactory(object):
     """Factory class for creating PSFs from a single image.
+
+    :param picker: Instance of PSFPicker, or subclass. Used to refine the
+        PSF model star list.
     """
-    def __init__(self, workDir, apRadPath):
+    def __init__(self, workDir, apRadPath, picker):
         super(PSFFactory, self).__init__()
         self.workDir = workDir
         if not os.path.exists(self.workDir): os.makedirs(workDir)
+        self.picker = picker
 
         # Install aperture radius config file
         self.apRadPath = os.path.join(self.workDir, "photo.opt")
@@ -65,25 +69,14 @@ class PSFFactory(object):
         apFilePath = self.daophot.get_path('last', 'ap')
         self.daophot.pickPSFStars(100, apPhot='last')
         
-        psfN, psfX, psfY = owl.dao.parseCoordFile(
-            self.daophot.get_path('last', 'lst'))
-        psfPoints = owl.region.PointList()
-        psfPoints.setFrame('image')
-        psfPoints.setPoints(psfX, psfY, size=15, shapes="diamond",
-                labels=None, colours="red")
-        psfPoints.writeTo(os.path.join(self.workDir,
-            self.imageName + "_psf.reg"))
-        
-        # Make custom picks
-        picker = StarPicker(self.daophot, 'last', self.imagePath)
-        picker.useDaophotPicks()
-        if self.flagPath is not None:
-            picker.filterOnFlagMap(self.flagPath)
-        picker.filterBright2MASSByDistance(40., 14., self.band)
-        pickPath = os.path.join(self.workDir, self.imageName + "rev.lst")
-        picker.write(pickPath)
-        picker.writeRegions(os.path.join(self.workDir,
-            self.imageName + "_psfrev.reg"))
+        # Make custom picks for PSF models
+        # TODO force use of lst; or allow the picker to work directly
+        # from apphot?
+        pickPath = os.path.join(self.workDir, self.imageName + "_rev.lst")
+        pickRegPath = os.path.join(self.workDir,
+                self.imageName + "_psfrev.reg")
+        self.picker(self.daophot, 'last', self.imagePath, pickPath,
+                regionPath=pickRegPath)
         
         # Make PSF, run allstar
         fitText, psfPath, neiPath = self.daophot.make_psf(apPhot='last',
@@ -100,14 +93,14 @@ class PSFFactory(object):
         for varPSF in range(0, maxVarPSF + 1):
             itername = "var%i" % varPSF
             try:
-                self._iteratePSF(varPSF, picker, neiPath, runAllstar,
+                self._iteratePSF(varPSF, neiPath, runAllstar,
                         name=itername)
             except PSFNotConverged:
                 varPSF = -1
-                self._makeAnalyticPSF(picker)
+                self._makeAnalyticPSF()
         
         # make final psf on clean image, keeping last-used varPSF
-        success = self._iteratePSF(varPSF, picker, neiPath, runAllstar,
+        success = self._iteratePSF(varPSF, neiPath, runAllstar,
                 name='fin')
         print success
         
@@ -121,12 +114,12 @@ class PSFFactory(object):
         
         return (psfPath, pickPath, coordFilePath, apFilePath)
     
-    def _makeAnalyticPSF(self, picker):
+    def _makeAnalyticPSF(self):
         """This is a bailout method to return the path to the analytic PSF.
         This is called whenever the empirical PSFs fail to converge."""
         print "FALLING BACK TO ANALYTIC PSF"
         self.daophot.set_option("VA", "-1")
-        pickPath = picker.getOutputPath()
+        pickPath = self.picker.get_output_path()
         fitText, psfPath, neiPath = self.daophot.make_psf(apPhot='last',
                 starList=pickPath, psfName='bail')
         self.daophot.shutdown()
@@ -142,7 +135,7 @@ class PSFFactory(object):
         neiSubPath = "_".join((imageRoot, itername, "subnei.fits"))
         return alsPath, alsStarSubPath, neiSubPath
     
-    def _iteratePSF(self, varPSF, starPicker, neiPath, runAllstar, name=None):
+    def _iteratePSF(self, varPSF, neiPath, runAllstar, name=None):
         """Performs a recipe of
         * subtract neighbouring stars
         * fit PSF
@@ -151,7 +144,7 @@ class PSFFactory(object):
         if name is None:
             name = str(varPSF)
         alsPath, alsStarSubPath, neiSubPath = self._makeAllstarPaths(name)
-        pickPath = starPicker.getOutputPath()
+        pickPath = self.picker.get_output_path()
 
         # had PSF fit be repeated; will reset to false if no stars are culled
         repeat = True
@@ -162,7 +155,7 @@ class PSFFactory(object):
             self.daophot.attach(neiSubPath)  # use the nei-subtracted image
             fitText, psfPath, neiPath = self.daophot.make_psf(apPhot='last',
                     starList=pickPath, psfName=name)
-            if starPicker.cullWithFitResults(fitText):
+            if self.picker.cull_with_fit_results(fitText):
                 repeat = True
                 print "repeating PSF fit after culling stars"
             else:
@@ -254,18 +247,19 @@ class PSFNotConverged(Exception): pass
 class StarPicker(object):
     """StarPicker is intended as a replacement for the built-in DAOPHOT/PICK.
     """
-    def __init__(self, daophot, daophotName, inputImagePath):
-        """
+    def __init__(self):
+        super(StarPicker, self).__init__()
+
+    def __call__(self, daophot, daophotName, outputPath, regionPath=None):
+        """Run the PSF star picking pipeline.
+        
         :param daophot: a `Daophot` instance of the image being worked on.
         :param daophotName: name that has the cached results (from, e.g. FIND,
             PHOTOMETRY) to be used to build the star lists. All types of
-            results should be cached in `daophot` under the same name.
+            results sould be cached in `daophot` under the same name.
         """
-        super(StarPicker, self).__init__()
         self.daophot = daophot
         self.daophotName = daophotName
-        self.inputImagePath = inputImagePath
-        self.psc = None  # 2MASS point source catalog
         self.outputPath = None  # where the lst will be saved
         
         # Load the aperture photometry of the star list
@@ -273,8 +267,19 @@ class StarPicker(object):
         # By default, accept all stars as PSF candidates
         self.candidates = self.apCatalog.getStarIDs()
         print "There are %i candidates on init" % len(self.candidates)
+
+        self.user_processing()
+
+        self.write(outputPath)
+
+        if regionPath is not None:
+            self.write_regions(regionPath)
+
+    def user_processing(self):
+        """This method should be implemented by the user."""
+        pass
     
-    def getOutputPath(self):
+    def get_output_path(self):
         return self.outputPath
     
     def write(self, outputPath):
@@ -290,7 +295,7 @@ class StarPicker(object):
         pickCatalog.write(outputPath)
         self.outputPath = outputPath
     
-    def writeRegions(self, outputPath):
+    def write_regions(self, outputPath):
         """Writes a DS9-compatible .reg file with the selected PSF model stars.
         """
         x = [self.apCatalog.stars[star]['x'] for star in self.candidates]
@@ -303,16 +308,7 @@ class StarPicker(object):
                 colours="cyan")
         psfPoints.writeTo(outputPath)
     
-    def useDaophotPicks(self):
-        """Whittles down the candidate list to just those selected by DAOPHOT
-        PICK.
-        """
-        pickCatalog = owl.dao.PickCatalog()
-        pickCatalog.readFromDaophot(self.daophot, self.daophotName)
-        self.candidates = pickCatalog.getStarIDs()
-        print "There are %i candidates on useDaophot" % len(self.candidates)
-    
-    def filterOnFlagMap(self, flagPath):
+    def filter_on_flagmap(self, flagPath):
         """Applies the flagmap to filtering the PSF template stars. Any star
         whose centroid lies upon a flagged (>0) pixel will be rejected
         from candidacy.
@@ -332,96 +328,7 @@ class StarPicker(object):
         print "There are %i candidates on flagmap filter" % \
                 len(self.candidates)
     
-    def _get2MASS(self):
-        """Sets the self.psc catalog with 2MASS stars in the input image frame.
-        """
-        inputFITS = pyfits.open(self.inputImagePath)
-        header = inputFITS[0].header
-        wcs = astWCS.WCS(header, mode='pyfits')
-        alphaNW, deltaNW = wcs.pix2wcs(2048, 2048)
-        alphaSW, deltaSW = wcs.pix2wcs(2048, 1)
-        alphaSE, deltaSE = wcs.pix2wcs(1, 1)
-        alphaNE, deltaNE = wcs.pix2wcs(1, 2048)
-        raMin = min(alphaNW, alphaSW, alphaNE, alphaNW)
-        raMax = max(alphaNW, alphaSW, alphaNE, alphaNW)
-        decMin = min(deltaNW, deltaSW, deltaNE, deltaNW)
-        decMax = max(deltaNW, deltaSW, deltaNE, deltaNW)
-        catalog2MASS = owl.twomicron.Catalog2MASS()
-        self.psc = catalog2MASS.getStarsInArea(raMin, raMax, decMin, decMax)
-        inputFITS.close()
-    
-    def filterBright2MASSByDistance(self, radius, magThreshold, band):
-        """Rejects stars that are a certain distance from bright 2MASS stars
-        that are known by the user to be saturated. This might be the
-        candidate star itself.
-        
-        I propose that 13th magnitude is a good cut-off for a star that you'd
-        want to be close to. Need to check this...
-        
-        :param radius: exclusion zone of bright 2MASS stars around the
-            PSF candidate, in pixels.
-        :param band: can either be "J" or "Ks"
-        """
-        radius = radius ** 2.  # work in squared distances
-        
-        if band == "J":
-            magKey = "jmag"
-        elif band == "Ks":
-            magKey = "kmag"
-        else:
-            magKey = None
-        
-        brightRA = []
-        brightDec = []
-        
-        # Get 2MASS stars and magnitudes in the native frame of the input image
-        if self.psc is None:
-            self._get2MASS()
-        
-        # Find stars brighter than threshold
-        n2MASS = len(self.psc['ra'])
-        for i in xrange(n2MASS):
-            if self.psc[magKey][i] < magThreshold:
-                # convert this to image frame with pointToImageFrame...
-                brightRA.append(self.psc['ra'][i])
-                brightDec.append(self.psc['dec'][i])
-        nBrights = len(brightRA)
-        brightX = numpy.zeros(nBrights)
-        brightY = numpy.zeros(nBrights)
-        inputFITS = pyfits.open(self.inputImagePath)
-        header = inputFITS[0].header
-        wcs = astWCS.WCS(header, mode='pyfits')
-        for i in xrange(nBrights):
-            x, y = wcs.wcs2pix(brightRA[i], brightDec[i])
-            brightX[i] = x
-            brightY[i] = y
-        
-        # Ask candidate if it is within radius pixels of a bright star
-        for star in self.candidates:
-            x = int(self.apCatalog.stars[star]['x'])
-            y = int(self.apCatalog.stars[star]['y'])
-            dist = (x - brightX) ** 2. + (y - brightY) ** 2.
-            # print "The closest bright star is %.2f pixels" % dist.min()
-            if len(numpy.where(dist < radius)[0]) > 0:
-                # there are bright stars nearby; need to delete this candidate
-                # print "There is a bright 2MASS star on/near %i" % star
-                # print self.candidates.index(star)
-                self.candidates.remove(star)
-        
-        print "There are %i candidates on 2MASS filter" % len(self.candidates)
-    
-    def filterByNeighbours(self):
-        """Rejects stars that have neighbours within a certain distance.
-        
-        This method is different from `filterBright2MASSByDistance` as that one
-        rejects a star if any bright 2MASS star exists within a radius about
-        a candidate---it may be the candidate itself. This method simply asks
-        if this given star has a bright neighbour and doesn't
-        *self obliterate*.
-        """
-        pass
-    
-    def cullWithFitResults(self, fitText):
+    def cull_with_fit_results(self, fitText):
         """Removes stars flagged in `fitText`, which is returned by `daophot`
         during the psf fitting process. The culled stars are removed from
         `self.candidates`, and a new .lst file is automatically save to
